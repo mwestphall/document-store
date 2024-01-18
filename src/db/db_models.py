b91ef2f457f325c239cde2fbaad647a56d11ca82
Matthew Westphall <westphall@wisc.edu>
2024-01-18 17:02:59 -0600

    add ingest script to bulk import pdfs from bibjson

--- src/db/db_models.py
+++ src/db/db_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String, Boolean, Integer, DateTime, func
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import DeclarativeBase
 from uuid import uuid4
@@ -15,6 +15,21 @@ class DbArticle(Base):
     xdd_doc_id = Column(String, unique=True, default=None)
     doi = Column(String, unique=True, default=None)
     bucket_name = Column(String)
+
+    page_width = Column(Integer, default=None)
+    page_height = Column(Integer, default=None)
+    page_count = Column(Integer, default=None)
+
+    ingest_date = Column(DateTime, nullable=False, server_default=func.now())
+    ingest_batch = Column(String, default=None)
+
+    @property
+    def doi_link(self):
+        return f"https://doi.org/{self.doi}" if self.doi else None
+
+    @property
+    def xdd_link(self):
+        return f"https://xdd.wisc.edu/api/articles?docid={self.xdd_doc_id}"
 
 class DbApiKey(Base):
     """ ORM Mapping for simple API key based authorization to access copyright-protected documents """
